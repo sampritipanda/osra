26456caddbceb6fd57f5094fa8cbd6c898e5b6a3
Sampriti Panda <sampdingo@hotmail.com>
2014-09-26 22:44:25 +0530

    Reduce duplication in Orphan model

--- app/models/orphan.rb
+++ app/models/orphan.rb
@@ -6,15 +6,15 @@ class Orphan < ActiveRecord::Base
 
   validates :name, presence: true
   validates :father_name, presence: true
-  validates :father_is_martyr, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
-  validates :father_date_of_death, presence: true, date_not_in_future: true
+  validates :father_is_martyr, boolean: true
+  validates :father_date_of_death, date_not_in_future: true
   validates :mother_name, presence: true
-  validates :mother_alive, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
-  validates :date_of_birth, presence: true, date_not_in_future: true
-  validates :gender, presence: true, inclusion: {in: %w(Male Female) } # TODO: DRY list of allowed values
+  validates :mother_alive, boolean: true
+  validates :date_of_birth, date_not_in_future: true
+  validates :gender, inclusion: {in: %w(Male Female) } # TODO: DRY list of allowed values
   validates :contact_number, presence: true
-  validates :sponsored_by_another_org, inclusion: {in: [true, false] }, exclusion: { in: [nil]}
-  validates :minor_siblings_count, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :sponsored_by_another_org, boolean: true
+  validates :minor_siblings_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :original_address, presence: true
   validates :current_address, presence: true
   validates :orphan_status, presence: true
@@ -24,12 +24,20 @@ class Orphan < ActiveRecord::Base
   has_one :current_address, foreign_key: 'orphan_current_address_id', class_name: 'Address'
   has_many :sponsorships
   has_many :sponsors, through: :sponsorships
-  
+
   belongs_to :orphan_status
   belongs_to :orphan_sponsorship_status
 
   accepts_nested_attributes_for :current_address, allow_destroy: true
   accepts_nested_attributes_for :original_address, allow_destroy: true
+
+  scope :active,
+        -> { Orphan.joins(:orphan_status).
+            where(orphan_statuses: { name: 'Active' }) }
+  scope :unsponsored,
+        -> { Orphan.joins(:orphan_sponsorship_status).
+            where(orphan_sponsorship_statuses: { name: 'Unsponsored' }) }
+
 
   def full_name
     [name, father_name].join(' ')
@@ -37,7 +45,6 @@ class Orphan < ActiveRecord::Base
 
   def orphans_dob_within_1yr_of_fathers_death
     # gestation is considered vaild if within 1 year of a fathers death
-    return unless valid_date?(father_date_of_death) && valid_date?(date_of_birth)
     if (father_date_of_death + 1.year) < date_of_birth
       errors.add(:date_of_birth, "date of birth must be within the gestation period of fathers death")
     end
@@ -52,22 +59,4 @@ class Orphan < ActiveRecord::Base
     unsponsored_status = OrphanSponsorshipStatus.find_by_name('Unsponsored')
     self.update!(orphan_sponsorship_status: unsponsored_status)
   end
-
-  scope :active,
-        -> { Orphan.joins(:orphan_status).
-            where(orphan_statuses: { name: 'Active' }) }
-  scope :unsponsored,
-        -> { Orphan.joins(:orphan_sponsorship_status).
-            where(orphan_sponsorship_statuses: { name: 'Unsponsored' }) }
- 
-  private
-
-  def valid_date? date
-    begin 
-      Date.parse(date.to_s)
-    rescue ArgumentError
-      return false
-    end
-  end
-
 end
